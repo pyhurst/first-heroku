ad2b50a2cf32b22ec27535e138ae1748d66d3f88
Philip Hurst <youngphil9@gmail.com>
2020-04-24 11:11:24 -0500

    addex express.static to public

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'))
